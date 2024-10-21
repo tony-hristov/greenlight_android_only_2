45999f40a00e7cb2d63aa9a8118c56e8764cfcc4
Tony Hristov <tony.hristov@alkami.com>
2024-10-21 12:13:34 -0500

    Kotlin 2 & Compose BOM

--- app/src/main/java/com/example/greenlight_android_only/Constants.kt
+++ app/src/main/java/com/example/greenlight_android_only/Constants.kt
@@ -7,6 +7,6 @@ class Constants {
         const val CHILD_ID = 20532941
         const val TOKEN_EXPIRES = 7200
         const val TOKEN_TYPE = "Bearer"
-        const val TOKEN_VALUE = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjkxNzIzNDcsImV4cCI6MTcyOTE3OTU0NywiaXNzIjoic3RhZ2UuZ3JlZW5saWdodC5nbC10ZWNoLmlvL2lkZW50aXR5IiwiYXVkIjoicGFydG5lcnMuZ2wtdGVjaC5pbyIsInN1YiI6IjIwNTMyOTQwIiwiYWN0Ijp7InN1YiI6ImlvLmdsLXRlY2gucGFydG5lcnMvVlNDVSJ9fQ.Ob6OYYVrTswuf2a3lb85lnhSYFWcsZcmXNaJ_wMgJHASevI7r3yobWvh0dcYqshN"
+        const val TOKEN_VALUE = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mjk1MzEzNDAsImV4cCI6MTcyOTUzODU0MCwiaXNzIjoic3RhZ2UuZ3JlZW5saWdodC5nbC10ZWNoLmlvL2lkZW50aXR5IiwiYXVkIjoicGFydG5lcnMuZ2wtdGVjaC5pbyIsInN1YiI6IjIwNTMyOTQwIiwiYWN0Ijp7InN1YiI6ImlvLmdsLXRlY2gucGFydG5lcnMvVlNDVSJ9fQ.33OiZ31niCOWdfxPe2XYDPXoCmvk-8HSdS3nyt-7PAS9Z-O4wbDhpuLZ7jDs9Ges"
     }
 }
--- app/src/main/java/com/example/greenlight_android_only/MainActivity.kt
+++ app/src/main/java/com/example/greenlight_android_only/MainActivity.kt
@@ -5,18 +5,22 @@ import AlkamiGreenlightErrorListener
 import AlkamiGreenlightEventListener
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.graphics.Color
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import me.greenlight.partner.GreenlightSDK
 import me.greenlight.partner.GreenlightSDKConfiguration
 import me.greenlight.partner.GreenlightSDKEnvironment
+import me.greenlight.partner.theming.GreenlightSDKColors
+import me.greenlight.partner.theming.GreenlightSDKComposeColors
+import me.greenlight.partner.theming.GreenlightSDKTheme
+import me.greenlight.partner.theming.defaultGreenlightSDKLightColors
+import me.greenlight.partner.theming.defaultGreenlightSDKShapes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -34,9 +38,28 @@ class MainActivity : AppCompatActivity() {
                     authentication = AlkamiGreenlightAuthenticator(),
                     errorListener = AlkamiGreenlightErrorListener(),
                     eventListener = AlkamiGreenlightEventListener(),
+                    theme = GreenlightSDKTheme(
+                        colors = GreenlightSDKColors(
+                            light = getLightThemeColors()
+                        ),
+                        shapes = defaultGreenlightSDKShapes(),
+                    )
                 )
             GreenlightSDK.init(configuration, context = applicationContext)
             GreenlightSDK.launchChildDashboard(this, Constants.FAMILY_ID, Constants.CHILD_ID)
         }
+    }
+
+    /**
+     * Get all the colors from the theme data to initialize a light theme
+     */
+    private fun getLightThemeColors(): GreenlightSDKComposeColors {
+        val defaults = defaultGreenlightSDKLightColors()
+        return defaults.copy(
+            // Surface colors //
+
+            // Screen Surface colors
+            screenSurface = Color(255, 250, 225, 255),
+        )
     }
 }
